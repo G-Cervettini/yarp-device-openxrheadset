e4174e2d4414d64c742b282813c0f1edfd074c80
Guglielmo Cervettini <101020439+G-Cervettini@users.noreply.github.com>
2022-10-18 13:03:25 +0200

    Fixed Sphere member vars to fit convention m_name

--- src/devices/openxrheadset/rendering/Sphere.h
+++ src/devices/openxrheadset/rendering/Sphere.h
@@ -21,12 +21,12 @@ public:
     ~Sphere() {}
 
     // getters/setters
-    float getRadius() const                 { return radius; }
-    int getPanAngle() const                 { return panAngle; }
-    int getTiltAngle() const                { return tiltAngle; }
-    unsigned int getDPT() const             { return dpt; }
-    int getSectorCount() const              { return sectorCount; }
-    int getStackCount() const               { return stackCount; }
+    float getRadius() const                 { return m_radius; }
+    int getPanAngle() const                 { return m_panAngle; }
+    int getTiltAngle() const                { return m_tiltAngle; }
+    unsigned int getDPT() const             { return m_dpt; }
+    int getSectorCount() const              { return m_sectorCount; }
+    int getStackCount() const               { return m_stackCount; }
     void set(float radius, int panAngle, int tiltAngle, unsigned int dpt, bool flip, bool smooth=true);
     void setRadius(float radius);
     void setPanAngle(int panAngle);
@@ -36,28 +36,28 @@ public:
     void setSmooth(bool smooth);
 
     // for vertex data
-    unsigned int getVertexCount() const                         { return (unsigned int)vertices.size() / 3; }
-    unsigned int getNormalCount() const                         { return (unsigned int)normals.size() / 3; }
-    unsigned int getTexCoordCount() const                       { return (unsigned int)texCoords.size() / 2; }
-    unsigned int getIndexCount() const                          { return (unsigned int)indices.size(); }
-    unsigned int getLineIndexCount() const                      { return (unsigned int)lineIndices.size(); }
+    unsigned int getVertexCount() const                         { return (unsigned int)m_vertices.size() / 3; }
+    unsigned int getNormalCount() const                         { return (unsigned int)m_normals.size() / 3; }
+    unsigned int getTexCoordCount() const                       { return (unsigned int)m_texCoords.size() / 2; }
+    unsigned int getIndexCount() const                          { return (unsigned int)m_indices.size(); }
+    unsigned int getLineIndexCount() const                      { return (unsigned int)m_lineIndices.size(); }
     unsigned int getTriangleCount() const                       { return getIndexCount() / 3; }
-    unsigned int getVertexSize() const                          { return (unsigned int)vertices.size() * sizeof(float); }
-    unsigned int getNormalSize() const                          { return (unsigned int)normals.size() * sizeof(float); }
-    unsigned int getTexCoordSize() const                        { return (unsigned int)texCoords.size() * sizeof(float); }
-    unsigned int getIndexSize() const                           { return (unsigned int)indices.size() * sizeof(unsigned int); }
-    unsigned int getLineIndexSize() const                       { return (unsigned int)lineIndices.size() * sizeof(unsigned int); }
-    const std::vector<float> getVertices() const                { return vertices; }
-    const float* getNormals() const                             { return normals.data(); }
-    const float* getTexCoords() const                           { return texCoords.data(); }
-    const std::vector<unsigned int> getIndices() const          { return indices; }
-    const std::vector<unsigned int> getLineIndices() const      { return lineIndices; }
+    unsigned int getVertexSize() const                          { return (unsigned int)m_vertices.size() * sizeof(float); }
+    unsigned int getNormalSize() const                          { return (unsigned int)m_normals.size() * sizeof(float); }
+    unsigned int getTexCoordSize() const                        { return (unsigned int)m_texCoords.size() * sizeof(float); }
+    unsigned int getIndexSize() const                           { return (unsigned int)m_indices.size() * sizeof(unsigned int); }
+    unsigned int getLineIndexSize() const                       { return (unsigned int)m_lineIndices.size() * sizeof(unsigned int); }
+    const std::vector<float> getVertices() const                { return m_vertices; }
+    const float* getNormals() const                             { return m_normals.data(); }
+    const float* getTexCoords() const                           { return m_texCoords.data(); }
+    const std::vector<unsigned int> getIndices() const          { return m_indices; }
+    const std::vector<unsigned int> getLineIndices() const      { return m_lineIndices; }
 
     // for interleaved vertices: V/N/T
     unsigned int getInterleavedVertexCount() const              { return getVertexCount(); }    // # of vertices
-    unsigned int getInterleavedVertexSize() const               { return (unsigned int)interleavedVertices.size() * sizeof(float); }    // # of bytes
-    int getInterleavedStride() const                            { return interleavedStride; }   // should be 32 bytes
-    const std::vector<float> getInterleavedVertices() const     { return interleavedVertices; }
+    unsigned int getInterleavedVertexSize() const               { return (unsigned int)m_interleavedVertices.size() * sizeof(float); }    // # of bytes
+    int getInterleavedStride() const                            { return m_interleavedStride; }   // should be 32 bytes
+    const std::vector<float> getInterleavedVertices() const     { return m_interleavedVertices; }
 
     // draw in VertexArray mode
     void draw() const;                                  // draw surface
@@ -82,24 +82,24 @@ private:
                                          float x3, float y3, float z3);
 
     // memeber vars
-    float radius;
-    int panAngle;
-    int tiltAngle;
-    unsigned int dpt;                       // Degrees Per Triangle (in both longitude and latitude)
-    bool smooth;
-    bool flip { false };
+    float m_radius;
+    int m_panAngle;
+    int m_tiltAngle;
+    unsigned int m_dpt;                                // Degrees Per Triangle (in both longitude and latitude)
+    bool m_smooth;
+    bool m_flip { false };
 
-    int sectorCount;                        // longitude, # of slices
-    int stackCount;                         // latitude, # of stacks
-    std::vector<float> vertices;
-    std::vector<float> normals;
-    std::vector<float> texCoords;
-    std::vector<unsigned int> indices;
-    std::vector<unsigned int> lineIndices;
+    int m_sectorCount;                        // longitude, # of slices
+    int m_stackCount;                         // latitude, # of stacks
+    std::vector<float> m_vertices;
+    std::vector<float> m_normals;
+    std::vector<float> m_texCoords;
+    std::vector<unsigned int> m_indices;
+    std::vector<unsigned int> m_lineIndices;
 
     // interleaved
-    std::vector<float> interleavedVertices;
-    int interleavedStride;                  // # of bytes to hop to the next vertex (should be 32 bytes)
+    std::vector<float> m_interleavedVertices;
+    int m_interleavedStride;                  // # of bytes to hop to the next vertex (should be 32 bytes)
 
 };
 
